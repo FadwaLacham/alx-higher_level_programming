3baf532a82f96b40a3abc85d3fe1a02f5b96b756
FadwaLacham <fadwa.lacham@gmail.com>
2024-03-17 19:13:57 -0700

    5. All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,7 +13,6 @@ if __name__ == '__main__':
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    state_name = sys.argv[4]
 
     db = MySQLdb.connect(
         host="localhost",
@@ -23,21 +22,13 @@ if __name__ == '__main__':
         db=database
     )
 
-    cursor = db.cursor()
-    query = """
-        SELECT cities.name
-        FROM cities
-        JOIN states ON cities.state_id = states.id
-        WHERE states.name = %s
-        ORDER BY cities.id ASC
-    """
-    cursor.execute(query, (state_name,))
-    cities = cursor.fetchall()
-    if cities:
-        cities_list = ", ".join([city[0] for city in cities])
-        print(cities_list)
-    else:
-        print("No cities found for state: {}".format(state_name))
-
-    cursor.close()
+    cur = db.cursor()
+    cur.execute("SELECT cities.name\
+                FROM cities LEFT JOIN states\
+                ON states.id = cities.state_id\
+                WHERE states.name = %s\
+                ORDER BY cities.id ASC", (sys.argv[4],))
+    rows = cur.fetchall()
+    print(", ".join([row[0] for row in rows]))
+    cur.close()
     db.close()
